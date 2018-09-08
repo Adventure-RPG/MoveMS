722e676698ad34655504b53fcf371f9b322c7a6b
Аллин Никита Евгеньевич <allinne@altarix.ru>
2018-09-09 00:02:24 +0300

    отрефакторил в тайпскрипт.

--- test/test.ts
+++ test/test.ts
@@ -1,49 +1,52 @@
 'use strict'
-var expect = require('chai').expect,
-    server = require('../index.js').server,
-    io = require('socket.io-client'),
-    ioOptions = {
-        transports: ['websocket'],
-        forceNew: true,
-        reconnection: false
-    },
-    testMsg = 'HelloWorld',
+import * as http from "http";
+import * as socketIo from 'socket.io-client';
+import * as app from '../src/index';
+import * as chai from 'chai';
+
+const ioOptions = {
+    transports: ['websocket'],
+    forceNew: true,
+    reconnection: false
+};
+
+let testMsg = 'HelloWorld',
     sender,
-    receiver
+    receiver;
 
 
 describe('Chat Events', function(){
     beforeEach(function(done){
 
         // connect two io clients
-        sender = io('http://localhost:3000/', ioOptions)
-        receiver = io('http://localhost:3000/', ioOptions)
+        sender = socketIo('http://localhost:3000/', ioOptions);
+        receiver = socketIo('http://localhost:3000/', ioOptions);
 
         // finish beforeEach setup
         done()
-    })
+    });
 
     afterEach(function(done){
 
         // disconnect io clients after each test
-        sender.disconnect()
-        receiver.disconnect()
-        done()
-    })
+        sender.disconnect();
+        receiver.disconnect();
+        done();
+    });
 
     describe('Message Events', function(){
         it('Clients should receive a message when the `message` event is emited.', function(done){
             sender.emit('message', testMsg)
             receiver.on('message', function(msg){
-                expect(msg).to.equal(testMsg)
-                done()
+                chai.expect(msg).to.equal(testMsg);
+                done();
             })
         })
-    })
+    });
 
     // describe('Move it', function(){
     //   it('On receive message with title move,', function(done){
     //     done();
     //   })
     // })
-})
+});
